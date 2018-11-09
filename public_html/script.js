eb94026d400e21ed82e41dbd1e6852107f8585eb
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-09 12:00:28 -0700

    added small js to home section

---
+++ public_html/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	scaleHeader()
+};
+
+function scaleHeader() {
+	let michael = document.querySelector("#scaleHead");
+	michael.classList.toggle('scale');
+	console.log('hi')
+}
