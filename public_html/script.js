0189574a28049f4b342327fecbd0035038e08332
Michael Bovee <michael.j.bovee@gmail.com>
2018-11-15 08:12:35 -0700

    trying to figure out scroll event, added form submit event

--- public_html/script.js
+++ public_html/script.js
@@ -1,8 +1,40 @@
+const slide = document.querySelectorAll('.slide-in');
+const scrollingBox = document.querySelector('.scrolling-box');
+const form = document.querySelector('form');
+
 window.onload = function(){
-	scaleThings()
+	scaleThings();
 };
 
 function scaleThings() {
 	let scale = document.querySelector("#scale");
 	scale.classList.toggle('scale');
 }
+
+function handleSubmit() {
+	let submitPara = document.createElement("p");
+	submitPara.innerText = "You submitted a form"
+	form.appendChild(submitPara)
+}
+
+/*function checkSlide(e) {
+	console.log(e)
+	}
+
+function debounce(func, wait = 20, immediate = true) {
+	let timeout;
+	return function() {
+		let context = this, args = arguments;
+		let later = function() {
+			timeout = null;
+			if(!immediate) func.apply(context, args);
+		};
+		let callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	}
+}
+
+scrollingBox.addEventListener('scroll', debounce(checkSlide));*/
+form.addEventListener('submit', handleSubmit);
